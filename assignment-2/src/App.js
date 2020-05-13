08b690d70c57056187fac8811449a8127af2fc2b
Rafael Esteller <rafael.esteller@playsportsnetwork.com>
2020-05-13 23:05:05 +0100

    Create Char component - display chars and handle deletion

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ValidationComponent from './components/ValidationComponent';
+import Char from './components/Char/Char'
 
 class App extends Component {
 
@@ -12,7 +13,22 @@ class App extends Component {
             {'userInput': event.target.value }                           )
     };
 
+    deleteCharHandler = (index) => {
+        const text = this.state.userInput.split('');
+        text.splice(index, 1)
+        const updatedText = text.join('')
+        this.setState({userInput:updatedText})
+    }
+
+    
     render() {
+        const charList = this.state.userInput.split('').map((char, index) => {
+            return <Char 
+            character={char} 
+            key={index}
+            clicked={() => this.deleteCharHandler(index)}
+            />
+        });
         return (
             <div className="App">
                 <h1>Second react assignment</h1>
@@ -23,6 +39,7 @@ class App extends Component {
                 value={this.state.userInput}/>
                 <p>{this.state.userInput}</p>
                 <ValidationComponent inputLength={this.state.userInput.length}/>
+                {charList}
             </div>
         );
     };
--- assignment-2/src/components/Char/Char.js
+++ assignment-2/src/components/Char/Char.js
@@ -1,3 +1,18 @@
-const char = () => {
+import React from  'react';
+const char = (props) => {
+    const style = {
+        display: 'inline-block',
+        padding: '60px',
+        margin: '60px',
+        border: '1px solid black',
+        textAlign: 'center'
+    };
     
+    return ( 
+        <div style={style} onClick={props.clicked}>
+            {props.character}
+        </div>
+    )
 }
+
+export default char
