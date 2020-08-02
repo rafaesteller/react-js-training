aa2d0938b97c83d4f5faa77f502e8f19d61aa951
Rafael Esteller <rafael.esteller@playsportsnetwork.com>
2020-08-02 18:17:00 +0100

    Reference a jsx element in any part of the component by using ref and initializse in constructor

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,6 +6,15 @@ import classes from './Person.css';
 
 class Person extends Component {
 
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef(); 
+  }
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+  }
+
   render( ) {
     console.log('[Person.js] rendering...');
   
@@ -15,7 +24,10 @@ class Person extends Component {
           I am {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name} />      
+        <input type="text"
+         onChange={this.props.changed} 
+         value={this.props.name}
+         ref={this.inputElementRef} />      
       </Fragment>
     );
   }
