322a017cb585e7485402e8966033ddadd719f9dd
Rafael Esteller <rafael.esteller@playsportsnetwork.com>
2020-08-02 17:41:32 +0100

    update state modifying current state should be done using a function, not directly in the setState parameter

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -36,7 +36,8 @@ class App extends Component {
       {id: 3, 'name': 'Jon', 'age': '59'},
     ],
     showPersons: false,
-    showCockpit: true
+    showCockpit: true,
+    changeCounter: 0
   };
 
   shouldComponentUpdate() {
@@ -68,9 +69,12 @@ class App extends Component {
     const persons = [...this.state.persons]
     persons[personIndex] = person
 
-    this.setState (
-        {
-          persons: persons
+    this.setState ( (prevState) =>
+        { return {
+          persons: persons,
+          changeCounter: prevState.changeCounter + 1
+        }
+          
         }
     )
   };
